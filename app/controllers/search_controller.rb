705c6aa8ebe4c38aacf968f50c08f796ab45807f
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 10:57:59 -0700

    add failing test for nrel_service all_by_zip_code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,18 @@
 class SearchController < ApplicationController
 
   def index
-    request = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest') do |faraday|
-      faraday.params['api_key'] = ENV['nrel_api_key']
-      faraday.params['format'] = 'json'
-      faraday.params['radius'] = '6'
-      faraday.params['format'] = 'json'
-      faraday.params['fuel_type'] = 'ELEC,LPG'
-      faraday.params['limit'] = 10
-      faraday.params['location'] = params[:zip_code]
-    end
+
+    nrel_service.all_by_zip_code(params[:zip_code])
+    
+    # request = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest') do |faraday|
+    #   faraday.params['api_key'] = ENV['nrel_api_key']
+    #   faraday.params['format'] = 'json'
+    #   faraday.params['radius'] = '6'
+    #   faraday.params['format'] = 'json'
+    #   faraday.params['fuel_type'] = 'ELEC,LPG'
+    #   faraday.params['limit'] = 10
+    #   faraday.params['location'] = params[:zip_code]
+    # end
 
     parsed_response = JSON.parse(request.body, symbolize_names: true)
     parsed_stations = parsed_response[:fuel_stations]
@@ -21,4 +24,9 @@ class SearchController < ApplicationController
     @stations = stations
   end
 
+  private
+
+  def nrel_service
+    @nrel_service ||= NrelService.new
+  end
 end
--- spec/models/nrel_service_spec.rb
+++ spec/models/nrel_service_spec.rb
@@ -5,4 +5,19 @@ describe NrelService do
   it "exists" do
     expect(subject).to be_a NrelService
   end
+
+  describe "instance_methods" do
+    context "#all_by_zip_code" do
+      it "searches for stations by zip_code and returns 10 stations within 6 miles" do
+        VCR.use_cassette('NrelService#all_by_zip_code') do
+          request = subject.all_by_zip_code
+          stations = JSON.parse(request.body)
+          station = stations.first
+
+          expect(stations.count).to eq 10
+          expect(station['distance']).to be < 6
+        end
+      end
+    end
+  end
 end
