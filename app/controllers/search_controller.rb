b036db2613c3ba795e532d036faada3a33baa6be
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 09:56:30 -0700

    first attempt at faraday request in search controller, wip

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,19 @@
 class SearchController < ApplicationController
 
   def index
-    
+    request = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest') do |faraday|
+      faraday.params['api_key'] = ENV['nrel_api_key']
+      faraday.params['format'] = 'json'
+      faraday.params['radius'] = '6'
+      faraday.params['format'] = 'json'
+      faraday.params['fuel_type'] = ['ELEC','LPG']
+      faraday.params['limit'] = 10
+      faraday.params['location'] = params[:q]
+    end
+
+    parsed_stations = JSON.parse(request.body, symbolize_headers: true)
+
+    @stations = parsed_stations
   end
+
 end
