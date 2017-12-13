a1c858987b16f4a59f20a21c54227f8f977dbce5
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 11:10:51 -0700

    remove faraday from controller to nrel_service

--- app/models/nrel_service.rb
+++ app/models/nrel_service.rb
@@ -4,21 +4,15 @@ class NrelService
   end
 
   def all_by_zip_code(zip_code)
-    # conn.get("?location=#{zip_code}")
-    conn.get '/nearest', { :location => "#{zip_code}" }
-  end
-
-  private
-
-  def conn
-    Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1") do |faraday|
+    Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest') do |faraday|
       faraday.params['api_key'] = ENV['nrel_api_key']
       faraday.params['format'] = 'json'
       faraday.params['radius'] = '6'
       faraday.params['fuel_type'] = 'ELEC,LPG'
       faraday.params['limit'] = 10
-      faraday.adapter Faraday.default_adapter
+      faraday.params['location'] = zip_code
     end
   end
+
 
 end
