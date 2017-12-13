043c5bc97e68aea45629470fe162a18128069a7f
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 10:33:46 -0700

    change symbolize_headers to symbolize_names becasue JSON doesnt have headers

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,20 +11,14 @@ class SearchController < ApplicationController
       faraday.params['location'] = params[:zip_code]
     end
 
-    parsed_response = JSON.parse(request.body, symbolize_headers: true)
-    parsed_stations = parsed_response['fuel_stations']
+    parsed_response = JSON.parse(request.body, symbolize_names: true)
+    parsed_stations = parsed_response[:fuel_stations]
 
     stations = parsed_stations.map do |raw_station|
       Station.new(raw_station)
     end
 
     @stations = stations
-  end
-
-  private
-
-  def strong_params
-    params.permit(:zip_code)
   end
 
 end
