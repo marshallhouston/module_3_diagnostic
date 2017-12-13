52917be2901c018d3cc2d31a81ff5161c4c191c0
Marshall Houston <marshall.houston@gmail.com>
2017-12-13 09:35:23 -0700

    update search by zip code test with assertion that 10 are on page; input field changed to id to target via css

--- spec/features/user_can_search_for_stations_by_zipcode_spec.rb
+++ spec/features/user_can_search_for_stations_by_zipcode_spec.rb
@@ -9,15 +9,18 @@ describe "User can search by zip_code" do
         # When I visit "/"
         visit "/"
         # And I fill in the search form with 80203
-        fill_in 'search-field', with: "80203"
+        fill_in 'q', with: "80203"
         # And I click "Locate"
         click_on "Locate"
         # Then I should be on page "/search" with parameters visible in the url
         expect(current_path).to eq("/search")
         # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+        within "stations" do
+          expect( all('.name').count ).to eq(10)
+        end
+
         # And the stations should be limited to Electric and Propane
         # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-
       end
     end
   end
